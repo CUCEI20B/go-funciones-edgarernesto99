1a7068187f0013ba9cce3cadecec3d23149c2c77
Edgar Guzman <edgar.guzman6009@alumnos.udg.mx>
2020-10-21 13:23:29 -0500

    Prueba01

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,17 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(s []int64) {
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s)-i-1; j++ {
+			if s[j] > s[j+1] {
+				aux := s[j]
+				s[j] = s[j+1]
+				s[j+1] = aux
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
